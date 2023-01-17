60fab92431c8d269dbf14f23fc3f9efd38e52054
tbhachu <92338991+tbhachu@users.noreply.github.com>
2023-01-17 11:03:36 -0800

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Advise it</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    
+    <h1>Advise-It</h1>
+    <h1>Home<h1>
+    
+    <form action="planPage.php">
+      <button>Create New</button>
+    </form>
+    <script src="script.js"></script>
+  </body>
+</html>
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+
+window.onload = uniqueToken;
+let out = document.getElementById('token');
+function uniqueToken() 
+{
+    var result = '';
+    var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 7; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    out.innerHTML = result;
+
+}
