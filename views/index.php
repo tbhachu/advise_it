94a16a4c98b484433932363f47aa64c52502aa97
tbhachu <sem.b1901@gmail.com>
2023-01-20 16:57:24 -0800

    Moved files into folder structure

--- views/index.php
+++ views/index.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Advise it</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/
+      css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/
+      iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/styles.css">
   </head>
   <body>
 
@@ -15,8 +18,8 @@
     <form action="planPage.php" method="post">
       <button>Create New</button>
     </form>
-    
-    <script src="script.js"></script>
+
+    <script src="../scripts/script.js"></script>
   </body>
 </html>
 
