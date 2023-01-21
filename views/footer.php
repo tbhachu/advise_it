1c926fa18022a121abf6fc2387fb55f70ff1c791
tbhachu <sem.b1901@gmail.com>
2023-01-20 20:56:08 -0800

    Added footer.php

---
+++ views/footer.php
@@ -0,0 +1,23 @@
+
+
+    <footer class="fixed-bottom bg-dark text-white pt-5 pb-4">
+        <div class="container text-center text-md-left">
+            <div class="row text-center text-md-left">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Links</h5>
+                        <a href="admin.php">Admin</a>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
+                        <p>GRC Auburn Center</p>
+                        <p>1221 D St NE, Auburn, WA 98002</p>
+                        <p>software@greenriver.edu</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="../scripts/script.js"></script>
+  </body>
+</html>
