1cb89053b193ecc71af145c1946b9f836fe39f41
tbhachu <sem.b1901@gmail.com>
2023-01-20 20:57:10 -0800

    Included header and footer.

--- views/index.php
+++ views/index.php
@@ -1,26 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Advise it</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/
-      css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/
-      iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/styles.css">
-  </head>
-  <body>
+    <?php
+      include 'header.php';
+    ?>
+  
+  
+    <div class="container">
+      <div class="container text-center">
+        <h1>Advise-It</h1>
+      </div>
+      <br>
+      <div class="container text-center">
+        <form action="planPage.php" method="post">
+          <button>Create New Plan</button>
+        </form>
+      </div>
 
-    <h1>Advise-It</h1>
-    <h1>Home<h1>
+    </div>
+    <?php
+      include 'footer.php';
+    ?>
+
     
-    <form action="planPage.php" method="post">
-      <button>Create New</button>
-    </form>
-
-    <script src="../scripts/script.js"></script>
-  </body>
-</html>
 
 
