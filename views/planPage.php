8a8439cd2c2a4c6939348cb5be46d3f2be51175d
tbhachu <sem.b1901@gmail.com>
2023-01-20 16:54:31 -0800

    Updating quarter boxes using Bootstrap

--- views/planPage.php
+++ views/planPage.php
@@ -4,54 +4,22 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <title>Advise-It</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/
+      css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/
+      iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/styles.css">
   </head>
   <body>
     
   <div class="container">
       <h1>Quarterly plan</h1>
-          <p>token:</p>
-          <div id='token'></div>
+      <p>Token: </p>
+      <div id='token'>
+        <!-- This div will display our generated token -->
+      </div>
   <div>
 
-  <div class="container">
-    <div class="row">
-      <div class="card">
-        <div class="container">
-        <h1><b>Fall</b></h1>
-        <input type="text" name="input" />
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="container">
-        <h1><b>Winter</b></h1>
-        <input type="text" name="input" />
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="container">
-        <h1><b>Spring</b></h1>
-        <input type="text" name="input" />
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="container">
-        <h1><b>Summer</b></h1>
-        <input type="text" name="input" />
-        </div>
-      </div>
-
-</div>
-  <div>
-
-  <script src="script.js"></script>
+  <script src="../scripts/script.js"></script>
   </body>
 </html>
