3bfe120248c306f0382ebf2a48b49c29df13afa2
tbhachu <sem.b1901@gmail.com>
2023-01-20 20:57:21 -0800

    Added header and footer.

--- views/planPage.php
+++ views/planPage.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Advise-It</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/
-      css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/
-      iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/styles.css">
-  </head>
-  <body>
+    <?php
+      include 'header.php';
+    ?>
     
-  <div class="container">
-      <h1>Quarterly plan</h1>
-      <p>Token: </p>
-      <div id='token'>
-        <!-- This div will display our generated token -->
-      </div>
-  <div>
+    <!-- Token -->
+    <div class="container">
+        <h1>Quarterly plan</h1>
+        <p>Token: </p>
+        <div id='token'>
+          <!-- This div will display our generated token -->
+        </div>
+    <div>
 
-  <script src="../scripts/script.js"></script>
-  </body>
-</html>
+    <!-- Four quarters representing Fall, Winter, Spring and Summer -->
+    <div class="row">
+      <div class="col">Fall</div>
+      <div class="col">Winter</div>
+      <div class="col">Spring</div>
+      <div class="col">Summer</div>
+    </div>
+
+
+    <?php
+      include 'footer.php';
+    ?>
