99e8d4822e7e3938e6be007f9344639d0a9b2360
tbhachu <sem.b1901@gmail.com>
2023-02-01 15:53:38 -0800

    Fixed sql insert command

--- functions/insert.php
+++ functions/insert.php
@@ -13,15 +13,13 @@
         $summer = $_POST["summer"];
 
         $sql = "INSERT INTO plans(id, token, fall_q, winter_q, spring_q, summer_q, time_updated) VALUES(DEFAULT, '$token', '$fall', '$winter', '$spring', '$summer', DEFAULT);";
-    $execute = mysqli_query($conn, $sql);
-    if($execute) {
-        echo "Plan saved successfully!";
-    } else {
-        echo "Something went wrong.";
-    }
+        $execute = mysqli_query($conn, $sql);
+        if($execute) {
+            echo "Plan saved successfully!";
+        } else {
+            echo "Something went wrong.";
+        }
     }
 
-    
-    
 
 ?>
